fad1ec89158fa81645388d6b8f0eef0b0f2d87c5
Sh4ke96 <Sh4ke96@gmail.com>
2021-04-30 02:10:58 +0200

    sticky navbar + header section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 const hamburger_btn = document.querySelector(".hamburger");
 const menu_list = document.querySelector(".header__menu");
+const body = document.querySelector("body");
+const navbar = document.querySelector(".header__navbar");
 
 hamburger_btn.addEventListener('click', function () {
     menu_list.classList.toggle('show');
     hamburger_btn.classList.toggle('is-active');
+    body.classList.toggle('blocked');
+})
+
+window.addEventListener("scroll", function (){
+    navbar.classList.toggle("sticky", window.scrollY > 0);
 })
