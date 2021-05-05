c4c023e50f0977dfdf5123877ce5cd6ff01c1ccf
Sh4ke96 <Sh4ke96@gmail.com>
2021-05-05 15:41:55 +0200

    about-section

--- js/main.js
+++ js/main.js
@@ -7,12 +7,20 @@ const navbar = document.querySelector(".header__navbar");
 const cases = document.querySelector("#cases");
 const deaths = document.querySelector("#deaths");
 const recovered = document.querySelector("#recovered");
+// const menu_link = document.querySelector(".header__link");
 
 hamburger_btn.addEventListener('click', function () {
   menu_list.classList.toggle('show');
   hamburger_btn.classList.toggle('is-active');
   body.classList.toggle('blocked');
 })
+
+document.querySelectorAll('.header__link')
+      .forEach(header__link => header__link.addEventListener('click', function () {
+            menu_list.classList.remove('show');
+            hamburger_btn.classList.toggle('is-active');
+            body.classList.remove('blocked');
+      }));
 
 window.addEventListener("scroll", function (){
   navbar.classList.toggle("sticky", window.scrollY > 0);
