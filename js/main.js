6dcf70cbf6622a738d297a460745f1fa5164441f
Sh4ke96 <Sh4ke96@gmail.com>
2021-05-20 03:33:18 +0200

    add active-navbar effect

--- js/main.js
+++ js/main.js
@@ -49,3 +49,23 @@ for (let i = 0; i < answers_li.length; i++) {
     } 
   });
 }
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".header__link");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+  navLi.forEach(li => {
+		li.classList.remove('active-navbar');
+		const href = li.getAttribute('href').substring(1);
+		if (href === current) {
+			li.classList.add('active-navbar');
+		}
+	});
+});
